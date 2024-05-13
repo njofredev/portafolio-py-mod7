7ee3b5b9109295fc54ea4b21219b780b11d16fc9
njofredev <nico.jofreandrade@gmail.com>
2024-05-13 13:11:58 -0400

    Fix portafolio-py-mod7/actividad1

--- actividad1/notasProject/notasApp/views.py
+++ actividad1/notasProject/notasApp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Nota
 
 def lista_notas(request):
-    notas = Nota.objects.all()
+    notas = Nota.objects.all() # Sentencia ORM para traer todos los objetos de Notas
     return render(request, 'notasApp/lista_notas.html', {'notas': notas})
